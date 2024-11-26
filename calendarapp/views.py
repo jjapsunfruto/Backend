c3d74f3a0795832be028e198dccc29edde9868fd
LeeJE12 <jjungeun2013@gmail.com>
2024-11-26 13:44:05 +0900

    feat: 집안일 완료 상태 변경

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -10,9 +10,35 @@ class CalendarView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, year, month):
+        user = request.user
         tasks = Housework.objects.filter(houseworkDate__year=year, houseworkDate__month=month)
+        tasks.filter(user=user)
+        
         serializer = HouseworkSerializer(tasks, many=True)
         return Response({
             'message': f"{year}-{month}의 집안일 목록입니다.",
             'data': serializer.data
+            })
+
+class HouseworkDoneView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, format=None):
+        user = request.user
+        housework = Housework.objects.get(houseworkId=request.data.get('houseworkId'))
+
+        if housework.user == user:
+
+            houseworkDone = housework.houseworkDone
+            if houseworkDone == True:
+                housework.houseworkDone = False
+            elif houseworkDone == False:
+                housework.houseworkDone = True
+
+            housework.save()
+            serializer = HouseworkSerializer(housework)
+
+            return Response({
+                'message': "집안일 완료 상태 변경 완료",
+                'data': serializer.data
             })
