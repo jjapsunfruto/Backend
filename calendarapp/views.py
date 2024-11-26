5b639f18b6959323e0972d018f43b9ff31d9bc9d
LeeJE12 <jjungeun2013@gmail.com>
2024-11-26 13:24:38 +0900

    fix: 집안일 캘린더 연-월 필터링

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -3,14 +3,16 @@ from Housework.serializers import HouseworkSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class CalendarView(APIView):
-    def get(self, request, date):
-        tasks = Housework.objects.filter(hoseworkDate = date)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, year, month):
+        tasks = Housework.objects.filter(houseworkDate__year=year, houseworkDate__month=month)
         serializer = HouseworkSerializer(tasks, many=True)
-        return Response(serializer.data)
-
-
-
-# Create your views here.
+        return Response({
+            'message': f"{year}-{month}의 집안일 목록입니다.",
+            'data': serializer.data
+            })
