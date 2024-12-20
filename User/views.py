15e2aea4072e24e98d29f9e688f3581e9e7e453c
이정은 <144866681+LeeJE12@users.noreply.github.com>
2024-12-21 03:26:34 +0900

    fix: urlencode 추가

--- User/views.py
+++ User/views.py
@@ -4,7 +4,7 @@ from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
-
+from urllib.parse import urlencode
 
 import os, requests, logging
 
